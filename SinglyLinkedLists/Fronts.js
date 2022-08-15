37b540913ef65513680831337e38eb19160c136b
maguina-sj <jomaguina90@gmail.com>
2022-08-14 18:57:03 -0700

    sll fronts done

--- SinglyLinkedLists/Fronts.js
+++ SinglyLinkedLists/Fronts.js
@@ -9,19 +9,38 @@ class SLL {
     constructor() {
         this.head = null;
     }
-    addFront(val) {
-    let new_node = new Node(val);
-
-    if(!this.head) {
-        this.head = new_node;
-        return this;
-    }
+    addFront(data) {
+    let new_node = new Node(data);
     new_node.next = this.head;
     this.head = new_node;
-    return this;
+    return this.head;
+    }
+    removeFront() {
+        if(this.head == null) {
+            return this.head;
+        }
+        let removedNode = this.head;
+        this.head = removedNode.next;
+        removedNode.next = null;
+        return this.head;
+    }
+    front() {
+        if(this.head ==null) {
+            return null;
+        } else {
+            return this.head.data;
+        }
+        
     }
 }
+let mySLL = new SLL();
+mySLL.addFront(10);
+mySLL.addFront(5);
+mySLL.addFront(3);
+console.log(mySLL);
+console.log(mySLL.front());
 
-SLL1 = new SLL()
-SLL1.addFront(18)
+// 
 
+// mySLL.removeFront()
+// console.log(mySLL)
